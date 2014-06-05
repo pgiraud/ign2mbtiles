fb051a4ec7779b2b3262ca17e1cc7e1d8df724a6
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-06-05 18:35:20 +0200

    A bit more tiles

--- ign2mbtiles.py
+++ ign2mbtiles.py
@@ -6,6 +6,6 @@ logging.basicConfig(level=logging.DEBUG)
 url = "http://gpp3-wxs.ign.fr/qunk4wvmc8pjw82h379if6sl/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&STYLE=normal&FORMAT=image/jpeg&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}.jpg"
 
 mb = MBTilesBuilder(cache=True, tiles_url=url, tiles_headers={'Referer': 'localhost'}, filepath="/home/pierre/Documents/MapBox/tiles/ign.mbtiles")
-mb.add_coverage(bbox=(8.7, 41.6, 9.2, 41.9), zoomlevels=[13, 15])
+mb.add_coverage(bbox=(8.7, 41.5, 9.2, 42.1), zoomlevels=[13, 15])
 
 mb.run(force=True)

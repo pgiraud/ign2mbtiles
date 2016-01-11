6dce9927c194b202fbcf5bddc11431f23fc2c279
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-01-11 18:34:55 +0100

    Load extents from geojson

--- ign2mbtiles.py
+++ ign2mbtiles.py
@@ -1,25 +1,24 @@
 import logging
+import geojson
+from shapely.geometry import shape
 from landez import MBTilesBuilder
 
 logging.basicConfig(level=logging.DEBUG)
 
-url = "http://gpp3-wxs.ign.fr/qunk4wvmc8pjw82h379if6sl/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&STYLE=normal&FORMAT=image/jpeg&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}.jpg"
+KEY = 'gnk8fnku3lwbxjz1fz34xx32'
 
-mb = MBTilesBuilder(cache=True, tiles_url=url, tiles_headers={'Referer': 'localhost'}, filepath="/Users/pierregiraud/Documents/MapBox/tiles/ign.mbtiles")
+url = "http://gpp3-wxs.ign.fr/%s/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&STYLE=normal&FORMAT=image/jpeg&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}.jpg" % KEY
 
-# corsica
-bboxes = [
-    (8.7, 41.5, 9.4, 42.1),
-    (9.3, 42.68, 9.497, 43.01),
-    (9.049, 42.64, 9.327, 42.73),
-    (9.294, 41.96, 9.57, 42.69),
-    (8.52, 41.86, 9.3, 42.63),
-    (8.657, 41.4, 9.43, 41.97),
-    (9.034, 41.32, 9.29, 41.48),
-]
+mb = MBTilesBuilder(cache=True, tiles_url=url, tiles_headers={'Referer': 'localhost'}, filepath="/home/pierre/ign.mbtiles")
+
+f = file('ign2mbtiles.geojson', 'r')
+boxes_as_geojson = f.read()
+features = geojson.loads(boxes_as_geojson).features
+
 zooms = [11, 13, 15]
 
-for bbox in bboxes:
+for feature in features:
+    bbox = shape(feature.geometry).bounds
     mb.add_coverage(bbox=bbox, zoomlevels=zooms)
 
 mb.run(force=True)
